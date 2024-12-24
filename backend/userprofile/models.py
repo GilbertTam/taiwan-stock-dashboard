2a3a68d6b0150f771fe1807ee334a680bc21ec49
Nat Lee <natlee.work@gmail.com>
2024-12-24 16:22:10 +0800

    [update] add userprofile app

---
+++ backend/userprofile/models.py
@@ -0,0 +1,20 @@
+from email.policy import default
+from django.db import models
+
+# Create your models here.
+
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    """Customized user profile."""
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
+    displayname = models.CharField(max_length=50)
+    description = models.TextField(blank=True, default='')
+
+    def __str__(self):
+        return f"{self.user.username} Profile"
+
+    class Meta:
+        verbose_name = '使用者資料（User Profile）'
+        verbose_name_plural = '使用者資料（User Profiles）'
