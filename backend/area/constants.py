3ebbad8dbefa8009bbde14ace67b472b8a7ba3e9
Nat Lee <natlee.work@gmail.com>
2025-03-14 15:09:12 +0800

    [update] add constants for region

---
+++ backend/area/constants.py
@@ -0,0 +1,24 @@
+
+AREA_EN_CH_MAP = {
+    'hokkaido':'北海道',
+    'touhoku':'東北',
+    'tokyo':'東京',
+    'chubu':'中部',
+    'hokuriku':'北陸',
+    'kansai':'關西',
+    'chugoku':'中國',
+    'shikoku':'四國',
+    'kyushu':'九州',
+}
+
+AREA_EN_JP_MAP = {
+    'hokkaido':'北海道',
+    'touhoku':'東北',
+    'tokyo':'東京',
+    'chubu':'中部',
+    'hokuriku':'北陸',
+    'kansai':'関西',
+    'chugoku':'中国',
+    'shikoku':'四国',
+    'kyushu':'九州',
+}
