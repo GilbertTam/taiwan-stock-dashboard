36967e7534bec2995cde1b89477813a043bbd997
Nat Lee <natlee.work@gmail.com>
2025-03-14 16:51:04 +0800

    [update] make `CH` and `JP` name unique in `Area`

--- backend/area/models.py
+++ backend/area/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Area(models.Model):
     """電力區域"""
     name = models.CharField(max_length=50, unique=True, help_text="區域名稱")
-    name_ch = models.CharField(max_length=50, help_text="區域名稱(中文)")
-    name_jp = models.CharField(max_length=50, help_text="區域名稱(日文)")
+    name_ch = models.CharField(max_length=50, unique=True, help_text="區域名稱(中文)")
+    name_jp = models.CharField(max_length=50, unique=True, help_text="區域名稱(日文)")
 
     class Meta:
         verbose_name = "電力區域"
