534fe21305b6eee3d16511b2c1bd51ac842c6ef7
Nat Lee <natlee.work@gmail.com>
2025-03-14 15:09:59 +0800

    [update] add spot market predict admin

--- backend/spot_market_predict/admin.py
+++ backend/spot_market_predict/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from spot_market_predict.models import QuickAreaPricePredict
 
-# Register your models here.
+@admin.register(QuickAreaPricePredict)
+class QuickAreaPricePredictAdmin(admin.ModelAdmin):
+    list_display = ('id', 'calculating_date', 'trade_date', 'time_code', 'area', 'price_5', 'price_50', 'price_95')
+    search_fields = ('trade_date', 'area__name')
+    list_filter = ('trade_date', 'area__name')
+    ordering = ('calculating_date', 'trade_date', 'area__name')
+    date_hierarchy = 'trade_date'
+    list_per_page = 48
