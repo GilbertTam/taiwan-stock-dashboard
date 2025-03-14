a9bcf0dfd4cc6759b27691d18ec3c85f2f9b3545
NatLee <natlee.work@gmail.com>
2025-03-15 01:31:22 +0800

    [update] add `area` API

--- backend/area/views.py
+++ backend/area/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-# Create your views here.
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
+from area.models import Area
+from area.serializers import AreaSerializer
+
+class AreaListView(APIView):
+    """
+    獲取所有電力區域列表
+    """
+    permission_classes = (IsAuthenticated,)
+
+    @swagger_auto_schema(
+        operation_summary="獲取電力區域列表",
+        operation_description="返回所有電力區域的詳細資訊，包括ID、名稱、中文名稱和日文名稱",
+        responses={
+            "200": openapi.Response(
+                description="成功獲取電力區域列表",
+                examples={
+                    "application/json": {
+                        "result": [
+                            {
+                                "id": 1,
+                                "name": "tokyo",
+                                "name_ch": "東京",
+                                "name_jp": "東京"
+                            },
+                            {
+                                "id": 2,
+                                "name": "shikoku",
+                                "name_ch": "四國",
+                                "name_jp": "四国"
+                            }
+                        ],
+                        "code": 0,
+                    }
+                },
+            )
+        },
+    )
+    def get(self, request, format=None):
+        """
+        獲取所有電力區域的列表
+        """
+        areas = Area.objects.all()
+        serializer = AreaSerializer(areas, many=True)
+
+        response_data = {
+            "result": serializer.data,
+            "code": 0
+        }
+        
+        return Response(response_data)
