f7c81e3837f8cc5626e4d8bd9cdc3a32049f6674
NatLee <natlee.work@gmail.com>
2026-01-11 15:30:40 +0800

    chore: remove database readiness wait from Docker entrypoint to streamline startup process

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -5,10 +5,6 @@ DEBUG_="$(tr [A-Z] [a-z] <<<"$DEBUG")"
 
 echo "> Debug mode: $DEBUG_"
 
-# Wait for the database to be ready
-echo "> Waiting for the database to be ready"
-/wait
-
 echo "> Django migrations"
 python manage.py makemigrations
 python manage.py migrate
