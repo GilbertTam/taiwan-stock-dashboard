b7151b4e653130b581b0b24f1afe22ad873b8948
NatLee <natlee.work@gmail.com>
2026-01-11 15:35:57 +0800

    refactor: simplify SpotTrade and AreaPrice models and serializers for improved clarity and maintainability

--- backend/spot_market/admin.py
+++ backend/spot_market/admin.py
@@ -1,19 +1,1 @@
 from django.contrib import admin
-from spot_market.models import SpotTrade, AreaPrice
-
-@admin.register(SpotTrade)
-class SpotTradeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'trade_date', 'time_code', 'sell_quantity', 'buy_quantity', 'contract_quantity', 'system_price')
-    search_fields = ('trade_date', 'time_code')
-    list_filter = ('trade_date', 'time_code')
-    ordering = ('trade_date', 'time_code')
-    date_hierarchy = 'trade_date'
-
-@admin.register(AreaPrice)
-class AreaPriceAdmin(admin.ModelAdmin):
-    list_display = ('id', 'spot_trade', 'area', 'price', 'avoidable_cost')
-    search_fields = ('spot_trade__trade_date', 'area__name')
-    list_filter = ('spot_trade__trade_date', 'area__name')
-    ordering = ('spot_trade__trade_date', 'area__name')
-    date_hierarchy = 'spot_trade__trade_date'
-    list_per_page = 48
--- backend/spot_market/serializers.py
+++ backend/spot_market/serializers.py
@@ -1,72 +1,15 @@
 from rest_framework import serializers
-from spot_market.models import SpotTrade, AreaPrice
-from area.models import Area
 
-class AreaPriceSerializer(serializers.ModelSerializer):
-    name = serializers.CharField(source='area.name', read_only=True)
-    name_ch = serializers.CharField(source='area.name_ch', read_only=True)
-    name_jp = serializers.CharField(source='area.name_jp', read_only=True)
-    
-    class Meta:
-        model = AreaPrice
-        fields = [
-            'name', 'name_ch', 'name_jp', 
-            'price', 'avoidable_cost'
-        ]
-
-class SpotTradeSerializer(serializers.ModelSerializer):
-    area_prices = AreaPriceSerializer(many=True, read_only=True)
-    
-    class Meta:
-        model = SpotTrade
-        fields = [
-            'id', 'trade_date', 'time_code', 'sell_quantity', 
-            'buy_quantity', 'contract_quantity', 'system_price', 
-            'area_prices'
-        ]
-
-class SpotTradeWithAreaPriceSerializer(serializers.ModelSerializer):
-    """包含特定區域價格的現貨交易序列化器"""
-    name = serializers.SerializerMethodField()
-    name_ch = serializers.SerializerMethodField()
-    name_jp = serializers.SerializerMethodField()
-    price = serializers.SerializerMethodField()
-    avoidable_cost = serializers.SerializerMethodField()
-    
-    class Meta:
-        model = SpotTrade
-        fields = [
-            'id', 'trade_date', 'time_code', 'sell_quantity', 
-            'buy_quantity', 'contract_quantity', 'system_price',
-            'name', 'name_ch', 'name_jp', 'price', 'avoidable_cost'
-        ]
-    
-    def __init__(self, *args, **kwargs):
-        # 從上下文中獲取區域
-        self.area = kwargs.pop('context', {}).get('area', None)
-        super().__init__(*args, **kwargs)
-    
-    def get_area_name(self, obj):
-        if hasattr(obj, 'area_price'):
-            return obj.area_price.area.name
-        return None
-    
-    def get_area_name_ch(self, obj):
-        if hasattr(obj, 'area_price'):
-            return obj.area_price.area.name_ch
-        return None
-    
-    def get_area_name_jp(self, obj):
-        if hasattr(obj, 'area_price'):
-            return obj.area_price.area.name_jp
-        return None
-    
-    def get_price(self, obj):
-        if hasattr(obj, 'area_price'):
-            return obj.area_price.price
-        return None
-    
-    def get_avoidable_cost(self, obj):
-        if hasattr(obj, 'area_price'):
-            return obj.area_price.avoidable_cost
-        return None
+class SpotTradeSerializer(serializers.Serializer):
+    id = serializers.CharField(read_only=True)
+    trade_date = serializers.DateField()
+    time_code = serializers.IntegerField()
+    sell_quantity = serializers.IntegerField()
+    buy_quantity = serializers.IntegerField()
+    contract_quantity = serializers.IntegerField()
+    system_price = serializers.FloatField()
+    name = serializers.CharField(help_text="Area Name")
+    name_ch = serializers.CharField(help_text="Area Name CH")
+    name_jp = serializers.CharField(help_text="Area Name JP")
+    price = serializers.FloatField()
+    avoidable_cost = serializers.FloatField(required=False)
